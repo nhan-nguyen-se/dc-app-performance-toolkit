618f156fe9d52c322fd043ddc47403c80004c0e4
Nhan Nguyen <nhan.nguyen@atlasauthority.com>
2022-01-07 18:44:40 +0700

    iframe app-specific test

--- app/extension/confluence/extension_ui.py
+++ app/extension/confluence/extension_ui.py
@@ -10,35 +10,36 @@ from util.conf import CONFLUENCE_SETTINGS
 
 def app_specific_action(webdriver, datasets):
     page = BasePage(webdriver)
-    if datasets['custom_pages']:
-        app_specific_page_id = datasets['custom_page_id']
 
-    # To run action as specific user uncomment code bellow.
-    # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
-    # just before test_2_selenium_z_log_out
-    # @print_timing("selenium_app_specific_user_login")
-    # def measure():
-    #     def app_specific_user_login(username='admin', password='admin'):
-    #         login_page = Login(webdriver)
-    #         login_page.delete_all_cookies()
-    #         login_page.go_to()
-    #         login_page.wait_for_page_loaded()
-    #         login_page.set_credentials(username=username, password=password)
-    #         login_page.click_login_button()
-    #         if login_page.is_first_login():
-    #             login_page.first_user_setup()
-    #         all_updates_page = AllUpdates(webdriver)
-    #         all_updates_page.wait_for_page_loaded()
-    #     app_specific_user_login(username='admin', password='admin')
-    # measure()
+    @print_timing("selenium_app_specific_user_login")
+    def login_admin_user():
+        def app_specific_user_login(username='admin', password='admin'):
+            login_page = Login(webdriver)
+            login_page.delete_all_cookies()
+            login_page.go_to()
+            login_page.wait_for_page_loaded()
+            login_page.set_credentials(username=username, password=password)
+            login_page.click_login_button()
+            if login_page.is_first_login():
+                login_page.first_user_setup()
+            all_updates_page = AllUpdates(webdriver)
+            all_updates_page.wait_for_page_loaded()
+        app_specific_user_login(username='admin', password='admin')
+    login_admin_user()
 
-    @print_timing("selenium_app_custom_action")
-    def measure():
+    @print_timing("iframe for confluence settings")
+    def iframe_settings():
+        page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/admin/plugins/iframed/config.action")
+        page.wait_until_visible((By.ID, "add-list"))
 
-        @print_timing("selenium_app_custom_action:view_page")
-        def sub_measure():
-            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={app_specific_page_id}")
-            page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
-        sub_measure()
-    measure()
+        ip_input = page.get_element((By.ID, "list-input"))
+        ip_input.send_keys("https://atlasauthority.com")
+
+        add_button = page.get_element((By.ID, "add-list"))
+        add_button.click()
+
+        page.wait_until_visible((By.XPATH, "//option[@value='https://atlasauthority.com']"))
+
+        submit_button = page.get_element((By.ID, "submit-button"))
+        submit_button.click()
+    iframe_settings()
